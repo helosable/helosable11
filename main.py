df06a0a0e0bfe55944f8e66f2623af789f724471
helosable <75098613+helosable@users.noreply.github.com>
2020-12-09 15:55:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sqlite3
-import json 
+import ijson 
 
 
 cnx= sqlite3.connect("main.db")
@@ -11,10 +11,13 @@ loop1=0
 
 def json_still_valid(js):
     try:
-        parse = json.loads(js)
-    except json.decoder.JSONDecodeError:
+        parse = list(ijson.basic_parse(js,multiple_values=True))
+    except ijson.common.IncompleteJSONError:
+        return False
+    except ijson.JSONError:
         return False
     return parse
+
 
 with open("access.log","r") as myfile:
     for line in myfile:
@@ -24,17 +27,17 @@ with open("access.log","r") as myfile:
             cur.execute("""INSERT INTO my_table (time) VALUES ('не получилось')""")
             cnx.commit()
             continue
-        time=row['time']
-        remote_addr=row['remote_addr']
-        remote_user=row['remote_user']
-        body_bytes_sent=row['body_bytes_sent']
-        request_time=row['request_time']
-        status=row['status']
-        request=row['request']
-        request_method=row['request_method']
-        http_referrer=row['http_referrer']
-        http_user_agent=row['http_user_agent']
-        proxy_host=row['proxy_host']
+        time=row[2][1]
+        remote_addr=row[4][1]
+        remote_user=row[6][1]
+        body_bytes_sent=row[8][1]
+        request_time=row[10][1]
+        status=row[12][1]
+        request=row[14][1]
+        request_method=row[16][1]
+        http_referrer=row[18][1]
+        http_user_agent=row[20][1]
+        proxy_host=row[22][1]
         cur.execute("""INSERT INTO my_table (time, 
         remote_addr, 
         remote_user,  
