c1f34fce996b37e83840b440df940078d1a8fc3f
helosable <75098613+helosable@users.noreply.github.com>
2020-12-09 14:47:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sqlite3
-import ijson 
+import json 
 
 
 cnx= sqlite3.connect("main.db")
@@ -18,35 +18,51 @@ def json_still_valid(js):
         return False
     return parse
 
-with open("access.log","r") as my_file:
-    for loop in my_file:
-        row = json_still_valid(loop)
-        if type(row)==list:  
-            for line,event in row:
-                my_list=[line,event]
-                if my_list[0]=='string':
-                    arr.append(event)
-                if len(arr)==11:
-                    cur.execute("""INSERT INTO my_table (
-                                time, 
-                                remote_addr, 
-                                remote_user,  
-                                body_bytes_sent, 
-                                request_time, 
-                                status, 
-                                request,
-                                request_method,
-                                http_referrer,
-                                http_user_agent, 
-                                proxy_host) VALUES (?,?,?,?,?,?,?,?,?,?,?)""",(arr))
-                    arr=[]
-                    loop1+=1
-                    if loop1 == 100000:  
-                        loop1 = 0 
-                        cnx.commit()
-        elif row==False:
-            print(loop)
+with open("access.log","r") as myfile:
+    for line in myfile:
+        row = json_still_valid(line)
+        if row == False:
+            print (line)
             cur.execute("""INSERT INTO my_table (time) VALUES ('не получилось')""")
+            cnx.commit()
+            continue
+        time=row['time']
+        remote_addr=row['remote_addr']
+        remote_user=row['remote_user']
+        body_bytes_sent=row['body_bytes_sent']
+        request_time=row['request_time']
+        status=row['status']
+        request=row['request']
+        request_method=row['request_method']
+        http_referrer=row['http_referrer']
+        http_user_agent=row['http_user_agent']
+        proxy_host=row['proxy_host']
+        cur.execute("""INSERT INTO my_table (time, 
+        remote_addr, 
+        remote_user,  
+        body_bytes_sent, 
+        request_time, 
+        status, 
+        request,
+        request_method,
+        http_referrer,
+        http_user_agent, 
+        proxy_host) VALUES (?,?,?,?,?,?,?,?,?,?,?)""",(
+        time,
+        remote_addr,
+        remote_user,
+        body_bytes_sent,
+        request_time,
+        status,
+        request,
+        request_method,
+        http_referrer,
+        http_user_agent,
+        proxy_host
+        ))
+        loop1+=1
+        if loop1==100000:
+            loop1=0
             cnx.commit()
     cnx.commit()
    
