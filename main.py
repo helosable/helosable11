10e91567e84cc6c694a55ecd80e86234a5c621b1
helosable <75098613+helosable@users.noreply.github.com>
2020-12-10 14:57:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,19 +12,7 @@ with open("access.log","r") as myfile:
             print (line)
             db.false_insert()
             continue            
-        for i in row:
-            time=i['time']
-            remote_addr=i['remote_addr']
-            remote_user=i['remote_user']
-            body_bytes_sent=i['body_bytes_sent']
-            request_time=i['request_time']
-            status=i['status']
-            request=i['request']
-            request_method=i['request_method']
-            http_referrer=i['http_referrer']
-            http_user_agent=i['http_user_agent']
-            proxy_host=i['proxy_host']
-        db.inserting(time,remote_addr,remote_user,body_bytes_sent,request_time,status,request,request_method,http_referrer,http_user_agent,proxy_host)
+        db.inserting(next(row))
         loop1+=1
         if loop1==100000:
             loop1=0
