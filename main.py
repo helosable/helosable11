2243301baa0d6e2ca6865a8a202747708fffc88a
helosable <75098613+helosable@users.noreply.github.com>
2020-12-13 22:42:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,24 @@
 import sqlite3
 import ijson 
 import parser_data_manager as dm
-import hashlib
 
 id=1
 
 
-
 data_manager = dm.Parser_data_manager("main.db")
 def json_still_valid(js):
-    try:
-        parse = ijson.items(js,"",multiple_values=True)
-    except ijson.common.IncompleteJSONError:
-        return False
-    except ijson.JSONError:
-        return False
+    parse=ijson.items(js,"",multiple_values=True)
     return parse
 
 with open("access.log","r") as myfile:
     for line in myfile:
         row = json_still_valid(line)
-        if row == False:
+        try:
+            next_row = next(row)
+        except ijson.common.IncompleteJSONError:
             print (line)
             data_manager.false_insert()
             continue
-        next_row = next(row)
         str_row = str(next_row)
         hashed = data_manager.hashing(str_row)
         data_manager.inserting(next_row,id)
