53f253fc932d6e36feeb4ea233722037b1b4555b
helosable <75098613+helosable@users.noreply.github.com>
2020-12-14 17:58:16 +0300

    Update 20201206_001.py

--- migrations/20201206_001.py
+++ migrations/20201206_001.py
@@ -1,8 +1,3 @@
-from yoyo import step
-
-_depends_ = {"20201206_000_initial.py"}
-
 steps = [
-    step("ALTER TABLE my_table ADD id VARCHAR AUTO_INCREMENT "),
     step("ALTER TABLE my_table ADD hash VARCHAR ")
 ]
