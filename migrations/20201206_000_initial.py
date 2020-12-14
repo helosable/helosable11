4fae1aa6f8e25d0b2c48190c549168226413e24a
helosable <75098613+helosable@users.noreply.github.com>
2020-12-14 17:27:10 +0300

    Update 20201206_000_initial.py

--- migrations/20201206_000_initial.py
+++ migrations/20201206_000_initial.py
@@ -3,5 +3,5 @@ from yoyo import step
 _depends_ = {}
 
 steps = [
-    step("CREATE TABLE my_table (time VARCHAR, remote_addr VARCHAR, remote_user VARCHAR, body_bytes_sent VARCHAR, request_time VARCHAR, status VARCHAR, request VARCHAR, request_method VARCHAR, http_referrer VARCHAR, http_user_agent VARCHAR, proxy_host VARCHAR)")
+    step("CREATE TABLE my_table (id int NOT NULL AUTO_INCREMENT, time VARCHAR, remote_addr VARCHAR, remote_user VARCHAR, body_bytes_sent VARCHAR, request_time VARCHAR, status VARCHAR, request VARCHAR, request_method VARCHAR, http_referrer VARCHAR, http_user_agent VARCHAR, proxy_host VARCHAR,PRIMARY KEY(id))")
 ]
