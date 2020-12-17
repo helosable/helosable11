b8eea307184d11ecdd72ffea4835da31f5075ae9
helosable <75098613+helosable@users.noreply.github.com>
2020-12-17 14:17:45 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,12 +1,18 @@
 import unittest
 import parser_data_manager
+import ijson
 
 
 data_manager=parser_data_manager.Parser_data_manager("main.db")
 
+
 class main(unittest.TestCase):
-    def test_work(self,val):
-        return self.assertEqual(data_manager.hash_val(val),data_manager.hash_val(val))
+    def test_hash(self):
+        assert(data_manager.false_insert_val())
+        with open("access.log","r") as myfile:
+            for line in myfile:
+                row=next(ijson.items(line,"",multiple_values=True))
+                assert(data_manager.insert_val(row))
 
 test=main()
-print(test.test_work("llll"))
+print(test.test_hash())
