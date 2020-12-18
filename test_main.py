4df13ceac88524adb41a3c4b133e4b2a4efed445
helosable <75098613+helosable@users.noreply.github.com>
2020-12-18 16:17:00 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -4,19 +4,12 @@ import ijson
 import hashlib
 
 
-test_hash=hashlib.sha256(str("pp").encode("utf-8")).hexdigest()
-data_manager=parser_data_manager.Parser_data_manager("main.db")
+hash1="5eb63bbbe01eeed093cb22bb8f5acdc3"
+
+dm=parser_data_manager.Parser_data_manager("main.db")
 
 
 class main(unittest.TestCase):
     def test_hash(self):
-        assert(data_manager.false_insert_val())
-        with open("access.log","r") as myfile:
-            for line in myfile:
-                row=next(ijson.items(line,"",multiple_values=True))
-                test_hash=hashlib.sha256(str(row).encode("utf-8")).hexdigest()
-                assert test_hash!=data_manager.hash_val(row)
-                assert row == data_manager.insert_val(row)
+        assert hash1==dm.hash_val("hello_world")
 
-test=main()
-print(test.test_hash())
