dbb6697ad8b26027875198ab240d92bf8f75f632
helosable <75098613+helosable@users.noreply.github.com>
2020-12-18 16:26:12 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -6,10 +6,10 @@ import hashlib
 
 hash1="5eb63bbbe01eeed093cb22bb8f5acdc3"
 
-dm=parser_data_manager.Parser_data_manager()
-
 
 class main(unittest.TestCase):
     def test_hash(self):
-        assert hash1==dm.hash_val("hello_world")
+        assert hash1==parser_data_manager.hash_val("hello_world")
+
+
 
