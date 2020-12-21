92f306a7b72ab4bb678f79d1e93c4a2e59972a3a
helosable <75098613+helosable@users.noreply.github.com>
2020-12-21 19:43:01 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -15,20 +15,22 @@ class main(unittest.TestCase):
     def test_insert(self):
         obj={"time": "2020-10-27T14:45:42+00:00", "remote_addr": "103.42.20.221", "remote_user": "03039", "body_bytes_sent": "162", "request_time": "0.000", "status": "301", "request": "POST /d4w/api/getNewBookingsLong HTTP/1.1", "request_method": "POST", "http_referrer": "-", "http_user_agent": "SQLAnywhere/16.0.0.2546", "proxy_host": "-" }
         dm.insert_val(obj)
-        dm._cur.execute("""SELECT * FROM my_table """)
+        dm._cur.execute("""SELECT time,
+                    remote_addr,
+                    remote_user,
+                    body_bytes_sent,
+                    request_time,
+                    status,
+                    request,
+                    request_method,
+                    http_referrer,
+                    http_user_agent,
+                    proxy_host FROM my_table """)
         row=dm._cur.fetchone()
         lst=[]
-        lst1=[]
-        loop=0
         for val in obj.values():
             lst.append(val)
-        for  val in row:
-            if loop==0 or loop == 12:
-                loop+=1
-                continue
-            lst1.append(val)
-            loop+=1
-        self.assertTrue(lst1==lst)
+        self.assertTrue(list(row)==lst)
 
 if __name__ == "__main__":
       unittest.main()
