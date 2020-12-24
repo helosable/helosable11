6c14d7cbe535116a36f2b800871637f981b1fd45
helosable <75098613+helosable@users.noreply.github.com>
2020-12-24 16:51:15 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,6 @@ from distutils.core import setup, Extension
 
 setup(name='test',
       version='1.0',
-       install_requires=['ijson', 'sqlite3','yoyo','hashlib','unittest']
+       install_requires=['ijson', 'sqlite3','yoyo','hashlib','unittest'],
+       scripts=["test_main"]
       )
