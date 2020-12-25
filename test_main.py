11d43dee75a66a2a20fec32144ebedc43cd4c16c
helosable <75098613+helosable@users.noreply.github.com>
2020-12-25 19:34:02 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -11,15 +11,18 @@ class main(unittest.TestCase):
         
     @classmethod
     def setUpClass(cls):
-        cls.backend = get_backend("sqlite:///test.db")
+        backend = get_backend("sqlite:///test.db")
         migrations = read_migrations("./migrations")
-        with cls.backend.lock():
-            cls.backend.apply_migrations(cls.backend.to_apply(migrations))
+        with backend.lock():
+            backend.apply_migrations(backend.to_apply(migrations))
         cls.dm = parser_data_manager.Parser_data_manager('test.db')
 
 
     def setUp(self):
-        self.dm._cur.execute("""DELETE FROM my_table""") 
+        self.dm._cur.execute("""DELETE FROM my_table""")
+        self.cnx = sqlite3.connect("test.db")
+        self.cur = self.cnx.cursor()
+        self.count = 0  
 
     
     def test_hash(self):
@@ -27,7 +30,7 @@ class main(unittest.TestCase):
     def test_insert(self):
         obj = {"time": "2020-10-27T14:45:42+00:00", "remote_addr": "103.42.20.221", "remote_user": "03039", "body_bytes_sent": "162", "request_time": "0.000", "status": "301", "request": "POST /d4w/api/getNewBookingsLong HTTP/1.1", "request_method": "POST", "http_referrer": "-", "http_user_agent": "SQLAnywhere/16.0.0.2546", "proxy_host": "-" }
         self.dm.insert_val(obj)
-        self.dm._cur.execute("""SELECT time,
+        self.cur.execute("""SELECT time,
                     remote_addr,
                     remote_user,
                     body_bytes_sent,
@@ -38,15 +41,15 @@ class main(unittest.TestCase):
                     http_referrer,
                     http_user_agent,
                     proxy_host FROM my_table """)
-        row = self.dm._cur.fetchone()
+        row = self.cur.fetchone()
         self.assertTrue(row==tuple(obj.values()))
 
     def test_double_insert(self): 
         obj = {"time": "2021-10-27T14:45:42+00:00", "remote_addr": "103.42.20.221", "remote_user": "03039", "body_bytes_sent": "162", "request_time": "0.000", "status": "301", "request": "POST /d4w/api/getNewBookingsLong HTTP/1.1", "request_method": "POST", "http_referrer": "-", "http_user_agent": "SQLAnywhere/16.0.0.2546", "proxy_host": "-" }
         for i in range(2):
             self.dm.insert_val(obj)
-        self.dm._cur.execute("""SELECT * FROM my_table""")
-        row = self.dm._cur.fetchall()
+        self.cur.execute("""SELECT * FROM my_table""")
+        row = self.dm.cur.fetchall()
         self.assertTrue(len(row) == 1)
         
         
