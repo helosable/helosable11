9995a3d0d44cc8936f1c62a97b5de7d67ec61f75
helosable <reningan1@gmail.com>
2020-12-28 23:04:37 +0300

    added exit and enter

--- main.py
+++ main.py
@@ -3,24 +3,25 @@ import ijson
 import parser_data_manager as dm
 
 
+
 data_manager = dm.Parser_data_manager("main.db")
 def json_still_valid(js):
-        try:
-            parse=ijson.items(js,"",multiple_values=True)
-        except ijson.common.IncompleteJSONError:
-            return False
-        except ijson.JSONError:
-            return False
-        return parse
+    try:
+        parse=next(ijson.items(js,"",multiple_values=True))
+    except ijson.common.IncompleteJSONError:
+        return False
+    return parse
+
 
 with open("access.log","r") as myfile:
     for line in myfile:
         row = json_still_valid(line)
-        if row == False:
+        if row==False:
             print (line)
-            data_manager.false_insert()
+            data_manager.false_insert_val()
             continue
-        data_manager.inserting(next(row))
+        data_manager.insert_val(row)
         
 
+            
         
--- migrations/20201212_000.py
+++ migrations/20201212_000.py
@@ -3,6 +3,5 @@ from yoyo import step
 _depends_ = {"20201206_000_initial.py"}
 
 steps = [
-    step("ALTER TABLE my_table ADD id VARCHAR AUTO_INCREMENT "),
-    step("ALTER TABLE my_table ADD hash VARCHAR ")
+    step("ALTER TABLE my_table ADD row_hash VARCHAR(35) ")
 ]
