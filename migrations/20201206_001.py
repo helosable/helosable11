164cf6ede62c23c31573d826c45494dd4e0c1c63
helosable <reningan1@gmail.com>
2021-01-07 00:19:02 +0300

    ///

--- migrations/20201206_001.py
+++ migrations/20201206_001.py
@@ -3,6 +3,5 @@ from yoyo import step
 _depends_ = {"20201206_000_initial.py"}
 
 steps = [
-    step("ALTER TABLE my_table ADD row_hash VARCHAR(35)"),
-    step("CREATE INDEX hash_idx ON my_table row_hash")
+    step("ALTER TABLE my_table ADD row_hash VARCHAR(35)")
 ]
---
+++ migrations/20210107_002.py
@@ -0,0 +1,7 @@
+from yoyo import step
+
+_depends_ = {"20201206_000_initial.py","20201206_001.py"}
+
+steps = [
+    step("CREATE INDEX hash_idx ON my_table row_hash")
+]
