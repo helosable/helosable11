008ce5a0bec1558d20e1ef2004ac770879bdea7c
helosable <reningan1@gmail.com>
2021-01-07 02:00:04 +0300

    .

--- log_analyzer/models/parser_data_manager.py
+++ log_analyzer/models/parser_data_manager.py
@@ -34,33 +34,32 @@ class Parser_data_manager:
 
     def insert_val(self, obj):
         hashed1 = self.hash_val(obj)
-        if self._compare(hashed1) is False:
-            obj = collections.OrderedDict(sorted(obj.items()))
-            self._cur.execute("""INSERT OR IGNORE INTO my_table (
-                time,
-                remote_addr,
-                remote_user,
-                body_bytes_sent,
-                request_time,
-                status,
-                request,
-                request_method,
-                http_referrer,
-                http_user_agent,
-                proxy_host,
-                row_hash) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)""", (
-                obj['time'],
-                obj['remote_addr'],
-                obj['remote_user'],
-                obj['body_bytes_sent'],
-                obj['request_time'],
-                obj['status'],
-                obj['request'],
-                obj['request_method'],
-                obj['http_referrer'],
-                obj['http_user_agent'],
-                obj['proxy_host'],
-                hashed1))
+        obj = collections.OrderedDict(sorted(obj.items()))
+        self._cur.execute("""INSERT OR IGNORE INTO my_table (
+            time,
+            remote_addr,
+            remote_user,
+            body_bytes_sent,
+            request_time,
+            status,
+            request,
+            request_method,
+            http_referrer,
+            http_user_agent,
+            proxy_host,
+            row_hash) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)""", (
+            obj['time'],
+            obj['remote_addr'],
+            obj['remote_user'],
+            obj['body_bytes_sent'],
+            obj['request_time'],
+            obj['status'],
+            obj['request'],
+            obj['request_method'],
+            obj['http_referrer'],
+            obj['http_user_agent'],
+            obj['proxy_host'],
+            hashed1))
         self._count = (self._count + 1) % 10000
         if self._count == 0:
             self._cnx.commit()
@@ -68,8 +67,3 @@ class Parser_data_manager:
     @staticmethod
     def hash_val(val):
         return hashlib.md5(str(val).encode("utf-8")).hexdigest()
-
-    def _compare(self, hash1):
-        self._cur.execute("""SELECT row_hash FROM my_table WHERE row_hash=?""", [hash1])
-        tab = self._cur.fetchall()
-        return len(tab) > 0
