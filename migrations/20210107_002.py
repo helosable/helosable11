8f4324b3a0bd50479c7b74a5b7a9b7e465f5e22e
helosable <reningan1@gmail.com>
2021-01-07 00:26:55 +0300

    .

--- migrations/20210107_002.py
+++ migrations/20210107_002.py
@@ -3,5 +3,5 @@ from yoyo import step
 _depends_ = {"20201206_000_initial.py","20201206_001.py"}
 
 steps = [
-    step("CREATE INDEX hash_idx ON my_table row_hash")
+    step("CREATE UNIQUE INDEX hash_idx ON my_table (row_hash)")
 ]
