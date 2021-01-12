479ee355619499456de6f7d9c4cd4ac5fec88841
helosable <reningan1@gmail.com>
2021-01-12 14:57:17 +0300

    .

--- log_analyzer/main.py
+++ log_analyzer/main.py
@@ -1,6 +1,6 @@
+import ijson
 from yoyo import read_migrations, get_backend
 from models.parser_data_manager import Parser_data_manager
-import ijson
 
 
 def json_still_valid(js):
@@ -17,31 +17,19 @@ def migrate():
         backend.apply_migrations(backend.to_apply(migrations))
 
 
-def main(file="access_mini_false.log", db="main.db"):
+def main():
     try:
-        with open(file, "r") as myfile:
-            with Parser_data_manager(db) as dm:
+        with open("access.log", "r") as myfile:
+            with Parser_data_manager("main.db") as dm:
                 for line in myfile:
                     row = json_still_valid(line)
                     if not row:
-                        row = {"time": "не получилось",
-               "remote_addr": "103.42.20.221",
-               "remote_user": "03039",
-               "body_bytes_sent": "162",
-               "request_time": "0.000",
-               "status": "301",
-               "request": "POST /d4w/api/getNewBookingsLong HTTP/1.1",
-               "request_method": "POST",
-               "http_referrer": "-",
-               "http_user_agent": "SQLAnywhere/16.0.0.2546",
-               "proxy_host": "-"}
                         print(line)
-                        dm.insert_val(row)
-                        continue
+                        dm.false_insert_val()
                     dm.insert_val(row)
 
     except FileNotFoundError:
-        print("file 'access.log' not found")
+        print("file not found")
     except Exception as e:
         print(repr(e))
 
