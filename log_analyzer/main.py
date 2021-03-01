a26c21cc0d314ee43fa61011919e746db941564e
helosable <reningan1@gmail.com>
2021-03-01 21:33:20 +0300

    ip report was added arg parse was added

--- log_analyzer/main.py
+++ log_analyzer/main.py
@@ -1,17 +1,23 @@
 import ijson
 from yoyo import read_migrations, get_backend
 from models.parser_data_manager import Parser_data_manager
-import sys
+import sys, getopt
 import sqlite3
 import numpy
+import jinja2
+import argparse
 
+parser = argparse.ArgumentParser(description='args')
+parser.add_argument('-f_t', '--f_time', type = str)
+parser.add_argument('-s_t', '--s_time', type = str)
+parser.add_argument('-f', '--log_file', type = str, default = 'access.log')
+parser.add_argument('-r', '-rep', '--rep', type = str)
+args = parser.parse_args()
 
-try:
-    first_time = sys.argv[1]
-    second_time = sys.argv[2]
-except IndexError:
-    pass
-
+with open('config.json', 'r') as config:
+    settings_data = next(ijson.items(config, '', multiple_values=True))
+db_name = settings_data['db']
+api_token = settings_data['api_token']
 
 def json_still_valid(js):
     try:
@@ -25,52 +31,6 @@ def migrate():
     migration = read_migrations("./migrations")
     with backend.lock():
         backend.apply_migrations(backend.to_apply(migration))
-    with sqlite3.connect('main.db') as cnx:
-        cur = cnx.cursor()
-        p = cur.execute("SELECT * FROM my_table")
-        p = cur.fetchall()
-        try:
-            cur.execute("""CREATE TABLE new_table (id INTEGER AUTO_INCREMENT,
-        time date,
-        remote_addr VARCHAR,
-        remote_user VARCHAR,
-        body_bytes_sent VARCHAR,
-        request_time VARCHAR,
-        status VARCHAR,
-        request VARCHAR,
-        request_method VARCHAR,
-        http_referrer VARCHAR,
-        http_user_agent VARCHAR,
-        proxy_host VARCHAR,
-        row_hash VARCHAR(35),
-        file_name VARCHAR,
-        PRIMARY KEY(id))""")
-        except sqlite3.OperationalError:
-            pass
-
-        try:
-            cur.execute("CREATE UNIQUE INDEX hash_unique_index_1 ON new_table(row_hash)")
-        except sqlite3.OperationalError:
-            pass
-        with Parser_data_manager('main.db') as dm:
-
-            for i in p:
-                obj = {'time': f'{i[0]}', 'remote_addr': f'{i[1]}', 'remote_user': f'{i[2]}',
-                    'body_bytes_sent': f'{i[3]}',
-                    'request_time': f'{i[4]}', 'status': f'{i[5]}', 'request' : f'{i[6]}', 
-                    'request_method': f'{i[7]}', 'http_referrer': f'{i[8]}',
-                    'http_user_agent': f'{i[9]}', 'proxy_host': f'{i[10]}', 
-                    'file_name': f'{i[12]}'}
-                if len(obj['time']) > 20:
-                    dm.insert_val(obj, obj['file_name'])
-        try:
-            cur.execute("DROP TABLE my_table")
-        except sqlite3.OperationalError :
-            pass
-        try:
-            cur.execute("ALTER TABLE new_table RENAME TO my_table")
-        except sqlite3.OperationalError :
-            pass
        
 
 
@@ -92,13 +52,23 @@ def main(db_name, file_name):
     except Exception as e:
         print(repr(e))
 
-def report(db_name):
-    with Parser_data_manager(db_name) as dm:
-        dm.report()
+def render(report_name, db_name = db_name):
+    with Parser_data_manager(db_name, args.f_time, args.s_time) as dm:
+        if report_name == 'ip_report':
+            rep = dm.ip_report()
+            file_name = 'ip_report'
+        if report_name == 'per_report':
+            rep = dm.per_report()
+            file_name = 'per_report'
+    headings = rep[0]
+    data = rep[1:]
+    with open(f'jinja/templates/{file_name}.html', 'w') as myfile:
+        myfile.write(jinja2.Environment(loader = jinja2.FileSystemLoader('jinja/templates')).get_template('base.html').render(heading = headings, data = data))
     
 
-
 if __name__ == "__main__":
-    # migrate()
-    # main("main.db", 'access.log')
-    report('main.db')
+    with Parser_data_manager(db_name) as dm:
+        dm.second_migration()
+    main(db_name, args.log_name)
+    render(args.rep)
+    print(args)
--- log_analyzer/models/parser_data_manager.py
+++ log_analyzer/models/parser_data_manager.py
@@ -7,7 +7,9 @@ import collections
 import numpy
 
 class Parser_data_manager:
-    def __init__(self, connection_string):
+    def __init__(self, connection_string, first_time='', second_time=''):
+        self.first_time = first_time
+        self.second_time = second_time
         try:
             self._cnx = sqlite3.connect(connection_string)
             self._cur = self._cnx.cursor()
@@ -15,6 +17,49 @@ class Parser_data_manager:
             self._error_count = 1
         except sqlite3.Error:
             print("Error connecting to database!")
+
+    def second_migration(self):
+        self._cur.execute("SELECT * FROM my_table")
+        p = self._cur.fetchall()
+        try:
+            self._cur.execute("""CREATE TABLE new_table (id INTEGER AUTO_INCREMENT,
+        time date,
+        remote_addr VARCHAR,
+        remote_user VARCHAR,
+        body_bytes_sent VARCHAR,
+        request_time VARCHAR,
+        status VARCHAR,
+        request VARCHAR,
+        request_method VARCHAR,
+        http_referrer VARCHAR,
+        http_user_agent VARCHAR,
+        proxy_host VARCHAR,
+        row_hash VARCHAR(35),
+        file_name VARCHAR,
+        PRIMARY KEY(id))""")
+        except sqlite3.OperationalError:
+            pass
+        try:
+            self._cur.execute("CREATE UNIQUE INDEX hash_unique_index_1 ON new_table(row_hash)")
+        except sqlite3.OperationalError:
+            pass
+        for i in p:
+            obj = {'time': f'{i[0]}', 'remote_addr': f'{i[1]}', 'remote_user': f'{i[2]}',
+                'body_bytes_sent': f'{i[3]}',
+                'request_time': f'{i[4]}', 'status': f'{i[5]}', 'request' : f'{i[6]}', 
+                'request_method': f'{i[7]}', 'http_referrer': f'{i[8]}',
+                'http_user_agent': f'{i[9]}', 'proxy_host': f'{i[10]}', 
+                'file_name': f'{i[12]}'}
+            if len(obj['time']) > 20:
+                self.insert_val(obj, obj['file_name'])
+        try:
+            self._cur.execute("DROP TABLE my_table")
+        except sqlite3.OperationalError :
+            pass
+        try:
+            self._cur.execute("ALTER TABLE new_table RENAME TO my_table")
+        except sqlite3.OperationalError :
+            pass
 
     def close(self):
         if self._cnx:
@@ -109,13 +154,15 @@ class Parser_data_manager:
         if self._count == 0:
             self._cnx.commit()
 
-    def report(self, first_time='2020-10-27 14:45:42', second_time='2020-10-27 14:45:43'):
+    def per_report(self):
+        first_time = self.first_time
+        second_time = self.second_time
         time_first = f"{first_time}"
         time_second = f"{second_time}"
         per_list = [50, 75, 95, 99]
         time_list = []
         c_1 = 0
-        self._cur.execute(f"SELECT request FROM my_table GROUP BY request ")
+        self._cur.execute(f"SELECT request FROM my_table WHERE time BETWEEN {time_first} AND {time_second} GROUP BY request ")
         func_name = list(self._cur.fetchall())
         time_list.append(['func_name', '50 per', '90 per', '95 per', '99 per'])
         for func in func_name:
@@ -147,6 +194,34 @@ class Parser_data_manager:
                 new_per_list.append(i1)
             time_list.append(new_per_list)
         return time_list
+
+    def ip_report(self):
+        first_time = self.first_time
+        second_time = self.second_time
+        time_first = f"{first_time}"
+        time_second = f"{second_time}"
+        rep_list = []
+        rep_list.append(['time', 'func', 'ip'])
+        self._cur.execute(f"SELECT time, request, remote_addr WHERE time BETWEEN {time_first} AND {time_second} FROM my_table")
+        ip_name = list(self._cur.fetchall())
+        for ip in ip_name:
+            func = ip[1]
+            new_func = ''
+            if ip == 'error':
+                continue
+            q = 1
+            for i in func:
+                for l in i :
+                    if q == 0:
+                        break
+                    if l == '?' or l == ';':
+                        l = ''
+                        q = 0
+                    else:
+                        new_func += l
+                    time_list = [ip[0], new_func, ip[2]]
+            rep_list.append(time_list)
+        return rep_list
 
     @staticmethod
     def hash_val(val):
